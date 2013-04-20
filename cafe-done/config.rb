97428b0d3e3d07c11107d1299b0ca0c89f3323ba
Sam Richard <snugug@gmail.com>
2013-04-20 15:40:00 -0400

    Updated Config.rb

--- cafe-done/config.rb
+++ cafe-done/config.rb
@@ -11,6 +11,7 @@ css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "js"
+fonts_dir = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
