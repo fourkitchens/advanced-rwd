b481a379ada1214180c92bb325ce5e151e1a82ea
Chris Ruppel <chris.ruppel@gmail.com>
2013-05-17 16:09:44 -0500

    Removing comments from CSS output.

--- cafe/config.rb
+++ cafe/config.rb
@@ -19,7 +19,7 @@ fonts_dir = "fonts"
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
