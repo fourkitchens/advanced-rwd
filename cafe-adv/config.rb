5546ecb1f0e26ced68ad392fb05187ad5db1727c
Ian Carrico <Ian@FourKitchens.com>
2013-05-17 13:48:19 -0500

    Fix minor issues with images, add in compass, etc etc

--- cafe-adv/config.rb
+++ cafe-adv/config.rb
@@ -10,7 +10,7 @@ require 'singularity-extras'
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "images"
+images_dir = "img"
 javascripts_dir = "js"
 fonts_dir = "fonts"
 
--- cafe/config.rb
+++ cafe/config.rb
@@ -8,7 +8,7 @@ require 'sassy-buttons'
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "images"
+images_dir = "img"
 javascripts_dir = "js"
 fonts_dir = "fonts"
 
